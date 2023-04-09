fc5eb1fc7a92efdff98835c38235d2c876bc8396
Amos <asidelnik@gmail.com>
2023-04-09 14:55:14 +0300

    Project started

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('app'));
+
+if (module.hot) {
+  module.hot.accept(function() {
+    window.location.reload();
+  });
+}
+//
